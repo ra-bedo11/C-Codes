64d1c7256ecf4c3f630362356b39dc9fa008590c
yoyo <yoyo@PC1>
2017-06-23 23:14:56 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int xx = 10;
+            //double ss = 3.43;
+            //double zz = xx % ss ;
+            //zz++;
+            //Console.WriteLine(zz);
+            //zz += 1;
+            //Console.WriteLine(zz);
+
+            //Console.WriteLine("Hr");
+
+            //Console.Title = "first apps";
+            //Console.Write("please Enter Ur Name : ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("ur Name is " + name);
+
+            //int x;
+            //int z;
+            //int y;
+            //Console.WriteLine("Enter first No");
+            //x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Scond No");
+            //y = Convert.ToInt16(Console.ReadLine());
+            //z = x + y;
+            ////Console.WriteLine(x + " + " + y + " = " + z);
+            //Console.WriteLine("result : {0} + {1} = {2}",x,y,z);
+            //Console.ReadKey();
+
+            //Console.Write("Please Enter First Name : ");
+            //string fname = Console.ReadLine();
+            //Console.Write("Please Enter Last Name : ");
+            //string lname = Console.ReadLine();
+            //Console.Write("Please Enter Age : ");
+            //decimal age = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine(" Full Name is : {0} \n age is : {1}", fname +" " + lname, age);
+            //Console.ReadKey();
+
+            //const double pi = 3.14159;
+            //Console.WriteLine("Plase Enter Area");
+            //double radius = Convert.ToDouble(Console.ReadLine());
+            //double area_circle = radius * pi * pi;
+            //Console.WriteLine("the area of circle {0} ",area_circle);
+            //Console.ReadKey();
+
+            //Console.Write("Please Enter Mark ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //if (x >= 50 && x <= 100)
+            //{
+            //    Console.WriteLine("Pass");
+            //}
+            //else if (x < 50 && x >= 0)
+            //{
+            //    Console.WriteLine("Failed");
+            //}
+            //else {
+            //    Console.WriteLine("Out of Range!");
+            //}
+
+            //bool i = false;
+            //bool j = false;
+            //if (!(i && j)) {
+            //    Console.WriteLine("the red is true");
+            //} else {
+            //    Console.WriteLine("the red is fslse");
+            //}
+
+            //Console.Write("Please Enter No1 : ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Please Enter No2 : ");
+            //double y = Convert.ToDouble(Console.ReadLine());
+
+            //if (x > y)
+            //{
+            //    Console.WriteLine("x Is Bigger");
+            //}
+            //else if (x > y)
+            //{
+            //    Console.WriteLine("y Is Bigger");
+            //}
+            //else {
+            //    Console.WriteLine("y = x");
+            //}
+
+            //int x = 10;
+            //if (x > 0) {
+            //    if (x > 5) {
+            //        Console.WriteLine("x < 5");
+            //    }
+            //    Console.WriteLine("x > 0");
+            //}
+
+            //Console.WriteLine("please enter color : ");
+            //char color = Convert.ToChar(Console.ReadLine());
+            //switch (color)
+            //{
+            //    case 'r':
+            //        Console.WriteLine("red");
+            //        break;
+            //    case 'b':
+            //        Console.WriteLine("red");
+            //        break;
+            //    case 'y':
+            //        Console.WriteLine("yellow");
+            //        break;
+            //    default:
+
+            //            Console.WriteLine("cant read color");
+            //        break;
+            //}
+            //Console.WriteLine("please enter states : ");
+            //char pass = Convert.ToChar(Console.ReadLine());
+            //switch (pass) {
+            //    case 'p':
+            //        Console.WriteLine("please enter mark : ");
+            //        int mark = Convert.ToInt16( Console.ReadLine());
+            //        //switch (mark) {
+            //    case 80:
+            //        Console.Write("A+ ");
+            //        break;
+            //    case 60:
+            //        Console.Write("A ");
+            //        break;
+            //    default:
+            //        Console.Write("B ");
+            //        break;
+            //}
+
+            //    if (mark <= 100 && mark >= 90)
+            //    {
+            //        Console.Write("A+ ");
+            //    }
+            //    else if (mark <= 89 && mark >= 80)
+            //    {
+            //        Console.Write("A ");
+            //    }
+            //    else if (mark <= 79 && mark >= 70)
+            //    {
+            //        Console.Write("B+");
+            //    }
+            //    else if (mark <= 69 && mark >= 60)
+            //    {
+            //        Console.Write("B");
+            //    }
+            //    else if (mark <= 59 && mark >= 50)
+            //    {
+            //        Console.Write("C");
+            //    }
+            //    else {
+            //        Console.Write(" the mark < 50 ");
+            //    }
+
+            //    Console.Write(" pass");
+            //    break;
+            //default:
+            //    Console.WriteLine("Failed");
+            //    break;
+            //}
+
+            //Console.WriteLine("Please Enter marke : ");
+            //int mark =Convert.ToInt16(Console.ReadLine());
+            //string state;
+            //state = (mark >= 50) ? "pass" : "failed";
+            //Console.WriteLine(state);
+            //Console.ReadKey();
+            //int x = 1;
+            //while (x <= 10) {
+            //    Console.WriteLine( "the x is {0}" ,x);
+            //    x++;
+            //}
+            //do {
+
+            //    Console.WriteLine("the x is {0}", x);
+            //    x++;
+            //} while (x <= 10);
+
+            //for (int x=0; x <=10;x++) {
+
+            //    if (x == 6) {
+            //        continue;
+            //    }
+            //    Console.WriteLine("the x is {0}", x);
+            //}
+
+            //int[] arr = new int[] { 1, 3, 4, 5, 7, 8, 9, 0 };
+            //foreach (int e in arr) {
+            //    Console.WriteLine("the arr {0}",e);
+
+            //int[] arr = new int[] { 1, 3, 4, 5, 7, 8, 9, 0 };
+            ////for (int x =0; x<arr.Length;x++) {
+            ////    Console.WriteLine("the arr {0}",arr[x]);
+            ////}
+            //foreach (int x in arr) {
+            //    Console.WriteLine(x);
+            //}
+            //Console.WriteLine(arr.Length);
+
+            //int[] arr = new int[4] { 1,2,3,4};
+            //foreach (int e in arr) {
+            //    Console.WriteLine(e);
+            //}
+
+            //int[,] arr = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            ////arr[0,0] =1;
+            ////arr[0,1] = 2;
+            ////arr[1,1] = 3;
+            ////arr[1,0] = 8;
+            ////arr[1, 2] = 7;
+            ////arr[2, 1] = 6;
+            ////arr[2, 2] = 5;
+            //foreach (int e in arr)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            string[,] color = new string[4, 3];
+            color[0, 0] = "blue";
+            color[0, 1] = "red";
+            color[0, 2] = "green";
+
+            color[1, 0] = "yellow";
+            color[1, 1] = "light";
+            color[1, 2] = "black";
+
+            color[2, 0] = "orange";
+            color[2, 1] = "white";
+            color[2, 2] = "yellow and black";
+            foreach (string x in color) {
+                Console.WriteLine(x);
+            }
+
+            //for (int x=1;x <= 10; x++) {
+            //    for (int y=1;y <= 10;y++) {
+            //        Console.WriteLine( x + " * " + y + " = " +  x * y);
+            //    }
+            //    Console.WriteLine("========");
+            //}
+
+            //for (int x = 1; x <= 10; x++)
+            //{
+            //    for (int y = 1; y <= x; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+
+            //for (int x = 10; x >= 0; x--)
+            //{
+            //    for (int y = 10; y >= x; y--)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //for (;;) {
+            //    Console.WriteLine("Infinty Loop");
+            //}
+        }
+    }
+}
